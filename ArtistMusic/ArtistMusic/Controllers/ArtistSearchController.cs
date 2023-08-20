90b69644fe49719058b52c0a1e63fdb4fe04d192
Raju <nirbhay.singh06@gmail.com>
2023-08-20 22:57:58 +0530

    All project First code

---
+++ ArtistMusic/ArtistMusic/Controllers/ArtistSearchController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+ 
+using Newtonsoft.Json;
+
+using ArtistMusic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtistMusic.Controllers
+{
+    public class ArtistSearchController : Controller
+    {
+        private const string MusicBrainzApiUrl = "https://musicbrainz.org/ws/2/";
+        private const string UserAgent = "YourApp/1.0";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> SearchResults(string artistName)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+
+                    var response = await client.GetStringAsync($"{MusicBrainzApiUrl}artist/?query=artist:{artistName}&fmt=json");
+                    var artists = JsonConvert.DeserializeObject<MusicBrainzArtistResponse>(response);
+
+                    if (artists.Artists.Count == 0)
+                    {
+                        ViewBag.ErrorMessage = "Artist not found.";
+                        return View();
+                    }
+
+                    var artistId = artists.Artists[0].Id;
+                    var releaseResponse = await client.GetStringAsync($"{MusicBrainzApiUrl}release/?artist={artistId}&fmt=json");
+                    var releases = JsonConvert.DeserializeObject<MusicBrainzReleaseResponse>(releaseResponse);
+
+                    var songs = new List<Song>();
+
+                    foreach (var release in releases.Releases)
+                    {
+                        var albumTitle = release.Title;
+
+                        // Handle the case when a release has no tracks
+                        if (release.Media == null)
+                        {
+                            songs.Add(new Song
+                            {
+                                Title = "No tracks available",
+                                AlbumTitle = albumTitle
+                            });
+                        }
+                        else
+                        {
+                            foreach (var medium in release.Media)
+                            {
+                                foreach (var track in medium.Tracks)
+                                {
+                                    songs.Add(new Song
+                                    {
+                                        Title = track.Title,
+                                        AlbumTitle = albumTitle
+                                    });
+                                }
+                            }
+                        }
+                    }
+
+                    ViewBag.ArtistName = artistName;
+                    ViewBag.Songs = songs;
+
+                    return View(songs);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "An error occurred.";
+                return View();
+            }
+        }
+    }
+}
---
+++ ArtistMusic/ArtistMusic/Models/MusicDetails.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+
+namespace ArtistMusic.Models
+{
+    public class MusicBrainzArtistResponse
+    {
+        [JsonProperty("artists")]
+        public List<MusicBrainzArtist> Artists { get; set; }
+    }
+
+    public class MusicBrainzArtist
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    //public class MusicBrainzReleaseResponse
+    //{
+    //    [JsonProperty("releases")]
+    //    public List<MusicBrainzRelease> Releases { get; set; }
+    //}
+
+    //public class MusicBrainzRelease
+    //{
+    //    // Define properties for album information
+    //}
+
+    public class Song
+    {
+        public string Title { get; set; }
+        public string AlbumTitle { get; set; }
+    }
+
+
+    public class MusicBrainzReleaseResponse
+    {
+        [JsonProperty("releases")]
+        public List<MusicBrainzRelease> Releases { get; set; }
+    }
+
+    public class MusicBrainzRelease
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("media")]
+        public List<MusicBrainzMedium> Media { get; set; }
+    }
+
+    public class MusicBrainzMedium
+    {
+        [JsonProperty("tracks")]
+        public List<MusicBrainzTrack> Tracks { get; set; }
+    }
+
+    public class MusicBrainzTrack
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+
+}
